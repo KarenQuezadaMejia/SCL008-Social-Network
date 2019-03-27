0f3edebd2e43cd9d0501b5e4bbb7674711fb8552
TaphieM <taffymayen@gmail.com>
2019-03-27 14:36:27 -0600

    estructura log in

---
+++ src/assets/view/templateCreate.js
@@ -0,0 +1,21 @@
+
+import{createAccount} from '../js/auth.js';
+
+export const templateCreate =() =>{
+    const containerCreate=document.createElement('div')          //esta despues se le pasa al html
+    const contentCreate= `<p>Crear cuenta </p>
+                          <button id='create'>Crear cuenta </button>`   //contenido del login
+
+
+//pasar el contenido al div
+containerCreate.innerHTML = contentCreate;
+const btn = containerCreate.querySelector('#create');   //querySelector busca donde yo le indique, puede buscar en hijos
+btn.addEventListener('click',()=>{
+    console.log(createAccount);
+})
+
+return containerCreate;
+}
+
+//exportar se utiliza en otro archivo
+//importar lo estoy utilizando de otro archivo
