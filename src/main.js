11cd1a1a5f7f6c31527c1728932db9436b5c7fd6
KarenQuezadaMejia <quezadakaren16@gmail.com>
2019-04-02 14:08:11 -0300

    change template para login con google

--- src/main.js
+++ src/main.js
@@ -1,10 +1,20 @@
-
 // llamado a la inicialización de la ruta  e inicializamos firebase
 
 import {initRouter} from './route.js';
 
 const init =()=>{
-    initRouter();
+    const config ={
+        apiKey: "AIzaSyD0i2UCeP2yUbDjNQhXyp7rXROjgwqaZd0",
+        authDomain: "social-network-booklover.firebaseapp.com",
+        databaseURL: "https://social-network-booklover.firebaseio.com",
+        projectId: "social-network-booklover",
+        storageBucket: "social-network-booklover.appspot.com",
+        messagingSenderId: "189372927325"
+      };
+
+      firebase.initializeApp(config);
+    
+      initRouter();
 }
 
-window.addEventListener('load',init);
+window.onload=init();
