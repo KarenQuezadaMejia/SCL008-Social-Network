4e4d7fbd25fb94369eeb0e4ffce34581f4697379
TaphieM <taffymayen@gmail.com>
2019-04-08 22:12:30 -0500

    se mejoraron comentarios de posts y wall

--- src/assets/js/posts.js
+++ src/assets/js/posts.js
@@ -3,8 +3,9 @@ import {checkPost} from '../js/validation.js'
 //var db = firebase.firestore();
 //Función que pasa la data del post a firestore  
 export const newPost=(userId,title,author,rate,opinion) =>{
-    if(checkPost(title,author)){   
-    return firebase.firestore().collection('posts').add({     //add sirve para que generé un id automático
+    if(checkPost(title,author)){  
+     var db = firebase.firestore(); 
+     db.collection('posts').add({     //add sirve para que generé un id automático
        userId: userId,
        title: title,
        author: author,
